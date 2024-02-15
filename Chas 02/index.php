91d989f144050fa2b2b12f18269fe0800493d758
Sandra <sandragukicka@gmail.com>
2024-02-15 18:00:10 +0100

    index.pho

---
+++ Chas 02/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+//Variables
+// $ 
+// Syntax
+//Variables always start with $
+//Variables can not start with number
+//Variables are case sensitive
+//Variables can only contain (A-z, 0-9)
+/* Moze i vaka komentar */
+
+
+$x = 5;
+$text = "Zdravo Mladi";
+$TEXT = "Zdravo Stari";
+?> 
